41453affcef981baeab40143bd976b8df2fcfdac
Josh Matthews <jmatth@nbcs.rutgers.edu>
2013-01-09 15:06:23 -0500

    Adding options to recursively check all sub-maildirs.
    Also giving better help output and handling some cases better.

--- maildircheck.py
+++ maildircheck.py
@@ -4,13 +4,27 @@ import sys
 import glob
 import re
 import shutil
+import glob
 from optparse import OptionParser
 
 def main():
-    parser = OptionParser()
+    (options, maildir) = checkArgs()
 
-    parser.add_option('-d', '--directory', dest='dirname', action='store',
-        type='string', help='Path to Maildir to check')
+    if options.recursive:
+        checkMaildirFolders(maildir, options.quiet, options.fix)
+
+    else:
+        maildir_contents = os.listdir(maildir)
+        if not ('cur' in maildir_contents and 'new' in maildir_contents and
+                'tmp' in maildir_contents):
+            print >> sys.stderr, ('Missing cur, new, and/or tmp. Are you' +
+                    'this is a maildir?')
+            sys.exit(1)
+        for subdir in ['cur/', 'new/', 'tmp/']:
+            checkMailSize(maildir + subdir, options.quiet, options.fix)
+
+def checkArgs():
+    parser = OptionParser(usage='%prog [options] path_to_maildir')
 
     parser.add_option('-f', '--fix', dest='fix', default=False,
         action='store_true', help='Execute the calculated mv commands')
@@ -18,28 +32,48 @@ def main():
     parser.add_option('-q', '--quiet', dest='quiet', default=False,
         action='store_true', help='Don\'t display any output.')
 
+    parser.add_option('-r', '--recursive', dest='recursive', default=False,
+        action='store_true', help='Recursively process all extra folders in' +
+            'the given maildir.')
+
     (options, args) = parser.parse_args()
 
-    if options.dirname == None:
-        print 'Must provide a path to the Maildir'
-        sys.exit()
+    # We only want the path as an argument.
+    if len(args) > 1:
+        parser.error('Too many arguments')
+
+    # We actually need the path as an argument.
+    if len(args) < 1:
+        parser.error('Please provide a path to the maildir')
+
+    # Technically not wrong, but a stupid combination of options
+    if options.quiet and not options.fix:
+        parser.error('Specifying -q without -f effectively does noting '+
+                'so I\'ll just exit here')
+
+    # Grab the maildir path
+    maildir = args[0]
 
     # Make sure the directory has a trailing slash.
-    if not options.dirname[len(options.dirname)-1] == '/':
-        options.dirname = options.dirname + '/'
+    if not maildir[len(maildir)-1] == '/':
+        maildir = maildir + '/'
 
-    maildir_contents = os.listdir(options.dirname)
+    return (options, maildir)
 
-    if not ('new' in maildir_contents and
-            'tmp' in maildir_contents and
-            'cur' in maildir_contents):
-        print 'Missing cur, new, and/or tmp. Are you sure this is a maildir?'
-        sys.exit()
+def checkMaildirFolders(dirpath, quiet=False, fix=False):
 
-    for subdir in ['new/','tmp/','cur/']:
-        checkmail(options.dirname + subdir, options.quiet, options.fix)
+    subdirs = glob.glob(dirpath + '.*')
 
-def checkmail(dirpath, quiet=True, autofix=False):
+    for subdir in subdirs:
+        if (os.path.isdir(subdir) and os.path.isfile(subdir +
+            '/maildirfolder')):
+            checkMaildirFolders(subdir + '/', quiet, fix)
+
+    for subdir in ['cur/', 'new/', 'tmp/']:
+        checkMailSize(dirpath + subdir, quiet, fix)
+
+
+def checkMailSize(dirpath, quiet=False, fix=False):
     mailRegex = re.compile('[0-9]{10}\.M.*,S=[0-9]+:2,S?')
     for mail in os.listdir(dirpath):
         if mailRegex.match(mail):
@@ -53,7 +87,9 @@ def checkmail(dirpath, quiet=True, autofix=False):
                     print ('mv ' + dirpath + mail + ' ' + dirpath +
                         mail.replace("S=" + reported_size, "S=" +
                             str(actual_size)))
-                if autofix:
+                if fix:
                     shutil.move(dirpath + mail, dirpath +
-                        mail.replace("S=" + reported_size, "S=" + actual_size))
+                        mail.replace("S=" + reported_size, "S=" +
+                            str(actual_size)))
+
 main()
